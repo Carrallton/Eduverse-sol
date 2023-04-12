c0880a8e7e129b37835b0a8edc79ce4b1faceab7
James Broover <45153127+Carrallton@users.noreply.github.com>
2023-04-13 01:12:31 +0300

    OZ ERC-20

--- Token.sol
+++ Token.sol
@@ -1,68 +1,54 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// Импортируем стандартные библиотеки
+// Импортируем необходимые библиотеки OpenZeppelin
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 // Создаем контракт для нашего токена
 contract MyToken is ERC20, Ownable {
-    using SafeMath for uint256;
-
-    // Создаем переменную для хранения стоимости курса в токенах
-    uint256 public coursePrice;
-
-    // Создаем событие для уведомления об изменении стоимости курса
-    event CoursePriceChanged(uint256 newPrice);
-
-    // Конструктор контракта
+    // Конструктор для создания токена
     constructor(uint256 initialSupply) ERC20("Eduverse", "EDV") {
         _mint(msg.sender, initialSupply);
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(_to != address(0), "Invalid address");
-        require(_value > 0, "Value must be greater than zero");
-        require(_value <= allowance[_from][msg.sender], "Not enough allowance");
-        require(balanceOf(_from) >= _value, "Not enough tokens");
-        
-        allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value);
-        _transfer(_from, _to, _value);
+    // Функция для добавления токенов на баланс указанного адреса
+    function mint(address account, uint256 amount) public onlyOwner {
+        _mint(account, amount);
+    }
+
+    // Функция для сжигания токенов с баланса отправителя
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    // Функция для перевода токенов с баланса отправителя на баланс получателя
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
         return true;
     }
 
-    // Функция для изменения стоимости курса
-    function setCoursePrice(uint256 _newPrice) public onlyOwner {
-        require(_newPrice > 0, "Price must be greater than zero");
-        coursePrice = _newPrice;
-        emit CoursePriceChanged(_newPrice);
+    // Функция для проверки баланса токенов на балансе указанного адреса
+    function balanceOf(address account) public view override returns (uint256) {
+        return super.balanceOf(account);
     }
 
-    // Функция для покупки курсов или учебников за токены
-    function buyCourse() public onlyValidAddress {
-        require(balanceOf(msg.sender) >= coursePrice, "Not enough tokens to buy the course");
-        _transfer(msg.sender, owner(), coursePrice);
-        // Здесь должен быть код для начисления курса пользователю
+    // Функция для проверки доступного количества токенов для перевода с баланса отправителя на баланс получателя
+    function allowance(address owner, address spender) public view override returns (uint256) {
+        return super.allowance(owner, spender);
     }
 
-    // Функция для безопасной передачи токена другому адресу
-    function safeTransfer(address _to, uint256 _value) public onlyValidAddress {
-        require(_to != address(0), "Invalid address");
-        require(_value > 0, "Value must be greater than zero");
-        require(balanceOf(msg.sender) >= _value, "Not enough tokens");
-        require(transfer(_to, _value), "Transfer failed");
+    // Функция для разрешения перевода токенов с баланса отправителя на баланс получателя
+    function approve(address spender, uint256 amount) public virtual override returns (bool) {
+        _approve(_msgSender(), spender, amount);
+        emit Approval(_msgSender(), spender, amount);
+        return true;
     }
 
-    // Модификатор для ограничения доступа только владельцу контракта
-    modifier onlyOwner() {
-        require(msg.sender == owner(), "Only owner can call this function");
-        _;
-    }
-
-    // Модификатор для проверки корректности ввода адреса
-    modifier onlyValidAddress() {
-        require(msg.sender != address(0), "Invalid address");
-        _;
+    // Функция для перевода токенов с баланса отправителя на баланс получателя при наличии разрешения
+    function transferFrom(address sender, address recipient, uint256 amount) public virtual override returns (bool) {
+        _transfer(sender, recipient, amount);
+        _approve(sender, _msgSender(), super.allowance(sender, _msgSender()) - amount);
+        return true;
     }
 }
